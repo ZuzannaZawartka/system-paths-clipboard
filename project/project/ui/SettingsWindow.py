6018ae44bbdd72e0ccb6449d5e2787ae0be111c4
Zuzanna Zawartka <60311564+ZuzannaZawartka@users.noreply.github.com>
2024-04-12 19:10:08 +0200

    feat: Added Window MainWindow,SettingsWindow and WindowManager with two views (main,settings)

---
+++ project/project/ui/SettingsWindow.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QPushButton
+from Window import Window
+from MainWindow import MainWindow
+from PyQt5.QtCore import pyqtSignal
+
+class SettingsWindow(Window):
+
+    def __init__(self):
+        super(SettingsWindow, self).__init__('Settings Window')
+
+        # Dodanie przycisku do powrotu do MainWindow
+        self.button_back = QPushButton('Back to Main')
+        self.layout.addWidget(self.button_back)
--- project/project/ui/main.py
+++ project/project/ui/main.py
@@ -1,34 +1,18 @@
-import tkinter as tk
-from pynput import keyboard
+import sys
+from PyQt5.QtWidgets import QApplication
+from WindowManager import WindowManager
 
-def on_press(key):
-    # Obsługa zdarzenia naciśnięcia klawisza
-    try:
-        # Pobierz nazwę klawisza
-        key_name = key.char
-    except AttributeError:
-        # Jeśli nie można pobrać nazwy (np. klawisz specjalny), użyj opisu
-        key_name = str(key)
+class AppManager:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.window_manager = WindowManager()
 
-    # Wyświetl informację o naciśniętym klawiszu
-    print(f'Key {key_name} pressed')
+    def run(self):
+        # Uruchom główne okno
+        self.window_manager.run()
+        sys.exit(self.app.exec_())
 
-def start_key_listener():
-    # Rozpocznij nasłuchiwanie zdarzeń klawiatury
-    listener = keyboard.Listener(on_press=on_press)
-    listener.start()
 
-def main():
-    # Tworzenie głównego okna aplikacji tkinter
-    root = tk.Tk()
-    root.title("Global Keyboard Listener")
-
-    # Przycisk rozpoczynający nasłuchiwanie klawiatury
-    start_button = tk.Button(root, text="Start Listening", command=start_key_listener)
-    start_button.pack(pady=20)
-
-    # Uruchomienie głównej pętli aplikacji tkinterdfsffdddd
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app_manager = AppManager()
+    app_manager.run()
