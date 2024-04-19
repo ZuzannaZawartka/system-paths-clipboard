8c10310724abf22e59ca9d490a680ec4ea8615a9
Zuzanna Zawartka <60311564+ZuzannaZawartka@users.noreply.github.com>
2024-04-19 16:43:22 +0200

    feat: line ecit moved to a separate class

--- project/ui/MainWindow.py
+++ project/ui/MainWindow.py
@@ -1,10 +1,6 @@
-from PyQt5.QtWidgets import (  # pylint: disable = no-name-in-module
-    QLineEdit,
-    QLabel,
-    QListWidget,
-)
 from .Window import Window
 from .MainWindowList import MainWindowList # pylint: disable = no-name-in-module
+from .MainWindowLineEdit import MainWindowLineEdit # pylint: disable = no-name-in-module
 
 class MainWindow(Window):
     """
@@ -26,23 +22,21 @@ class MainWindow(Window):
         Ustawia tytuł okna i inicjuje interfejs użytkownika.
         """
         super(MainWindow, self).__init__("Main Window")
-        self.main_window_list = MainWindowList(self)
+        self.main_window_line_edit = MainWindowLineEdit(self)
+        self.main_window_list = MainWindowList(self,self.main_window_line_edit)
+    
         self.init_ui()
 
     def init_ui(self):
         """
         Ustawia interfejs użytkownika dla głównego okna.
 
-        Tworzy etykietę, pole do wprowadzania tekstu oraz listę opcji.
         """
-        # Etykieta do wyświetlania tekstu
-        self.label = QLabel("Edit:")
-        self.layout.addWidget(self.label)
-
-        # Pole do wprowadzania tekstu
-        self.text_edit = QLineEdit()
-        self.layout.addWidget(self.text_edit)
+        # Lista opcji wyświetlana w oknie
+        self.main_window_line_edit.init_line_edit()
 
         # Lista opcji wyświetlana w oknie
         self.main_window_list.init_list_widget()
 
+
+    
--- project/ui/MainWindowList.py
+++ project/ui/MainWindowList.py
@@ -1,9 +1,10 @@
 from PyQt5.QtWidgets import QListWidget # pylint: disable = no-name-in-module
 
 class MainWindowList:
-    def __init__(self, parent):
+    def __init__(self, parent,main_window_line_edit):
         self.parent = parent
         self.list_widget = QListWidget()
+        self.line_edit = main_window_line_edit
        
 
     def init_list_widget(self,new_items = []):
@@ -13,7 +14,7 @@ class MainWindowList:
         self.parent.layout.addWidget(self.list_widget)
 
         if(len(new_items) > 0):
-           self.parent.text_edit.setText(new_items[0])
+           self.line_edit.set_text(new_items[0])
 
     def refresh_list_widget(self, new_items):
         # Usunięcie istniejących elementów
@@ -37,4 +38,4 @@ class MainWindowList:
 
         Ustawia tekst pola do wprowadzania tekstu na wybrany element z listy.
         """
-        self.parent.text_edit.setText(self.list_widget.currentItem().text())
+        self.line_edit.set_text(self.list_widget.currentItem().text())
